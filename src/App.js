5ceff90315f00b4be47e637c257d84a61fe708a3
Alisson <alissonalves29@gmail.com>
2023-07-23 19:42:24 -0300

    button with gradient border

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './index.css';
+
+function App() {
+  return (
+    <div className='h-screen'>
+      <div className=' flex justify-center mt-52'>
+        <div className='flex justify-center p-1 m-3 w-20 shine-border rounded-full'>
+          <div className='cursor-pointer rounded-full bg-black text-white text-5xl w-full h-max flex justify-center m-auto align-middle'>
+            ola
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
